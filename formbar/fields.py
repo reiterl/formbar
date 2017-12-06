3d1f7c9502fab198cc49914a7bf1d378de8365d8
Ludwig Reiter <ludwig.reiter@intevation.de>
2017-12-06 14:45:01 +0100

    Fix BooleanSeletionField. v / option[1] is a string like 'True'/'False' and should be converted to bool. See issue3169.

--- formbar/fields.py
+++ formbar/fields.py
@@ -760,7 +760,7 @@ class SelectionField(CollectionField):
                 elif isinstance(self, IntSelectionField):
                     value = int(option[1])
                 elif isinstance(self, BooleanSelectionField):
-                    value = bool(option[1])
+                    value = self._str_to_bool(option[1])
                 else:
                     value = option[1]
                 options.append((option[0], value, option[2]))
@@ -801,7 +801,7 @@ class SelectionField(CollectionField):
             if isinstance(self, IntSelectionField):
                 value = int(v)
             elif isinstance(self, BooleanSelectionField):
-                value = bool(v)
+                value = self._str_to_bool(v)
             else:
                 value = unicode(v)
             serialized.append(value)
@@ -814,6 +814,13 @@ class SelectionField(CollectionField):
         # will be converted into the _string_ "{1,2,''}".
         return value
 
+    def _str_to_bool(self, string):
+        if string is None:
+            return None
+        elif string == "False":
+            return False
+        else:
+            return True
 
 class IntSelectionField(SelectionField):
     """Field which can have one or more of predefined values. The
